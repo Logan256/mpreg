4d09f2c11ce86967f05927f56de0f2bc7da090b7
Robert Morelli <robertondino@outlook.com>
2025-09-12 23:45:37 -0600

    Create robust cli - add std io handling - add diverse emojis - add file cli args - add diversity and vanilla flags

--- mpreg.py
+++ mpreg.py
@@ -1,27 +1,83 @@
+import argparse
+import sys
+import random
+from typing import Generator
 
+## Generators
 
-def mpreg(file_path: str, diversity: bool):
+# Incubators
+def diversity() -> Generator[str, None, None]:
+    variants = [
+        '🫃🏻','🫃🏼','🫃🏽','🫃🏾','🫃🏿',
+        '🫄🏻','🫄🏼','🫄🏽','🫄🏾','🫄🏿',
+        '🤰🏽','🤰🏾','🤰🏿']
+    while True:
+        random.shuffle(variants)
+        for variant in variants:
+            yield variant
 
-    lines = []
+def vanilla() -> Generator[str, None, None]:
+    while True:
+        yield '🫃'
+
+# Readers
+def read_file(file_path: str) -> Generator[str, None, None]:
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
-            lines = file.readlines()               
+            for ch in file.read(4096):
+                yield ch
     except FileNotFoundError:
-        print(f"Error: The file '{file_path}' was not found.")
-    except Exception as e:
-        print(e)
+        print("mpreg aborted [read error]: file not found", file=sys.stderr)
+    except Exception as err:
+        print("mpreg aborted [read error]: " + str(err), file=sys.stderr)
 
-    for i in range(len(lines)):
-        lines[i] = ''.join('🫃' if c == '—' or c == '-' else c for c in lines[i])
+def read_std_in() -> Generator[str, None, None]:
+    for ch in sys.stdin.read(4096):
+        yield ch
 
+## Writers
+def write_file(file_path: str, input_gen: Generator[str, None, None]):
     try:
-        with open(file_path, 'w', encoding='utf-8') as file:
-            for line in lines:
-                file.write(line)
-    except Exception as e:
-        print(e)
+        with open(file_path, "wb") as file:
+            for ch in input_gen:
+                file.write(bytes(ch, encoding='utf-8'))
+                file.flush()
+    except Exception as err:
+        print("mpreg aborted [write error]: " + str(err), file=sys.stderr)
+
+def write_std_out(input_gen: Generator[str, None, None]):
+    for ch in input_gen:
+        sys.stdout.buffer.write(bytes(ch, encoding='utf-8'))
+        sys.stdout.flush()
+
+
+## Compositer
+def mpregnate(input_gen: Generator[str, None, None], preg_gen: Generator[str, None, None]):
+    for ch in input_gen:
+        if ch != '—':
+            yield ch
+        else:
+            yield next(preg_gen)
+
+## Orchestration
+def read_flags():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", default="-")
+    parser.add_argument("-o", "--output", default="-")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-d", "--diversity", action="store_true", help="use diverse emoji")
+    group.add_argument("-v", "--vanilla", action="store_true", help="use single emoji")
+    return parser.parse_args()
+
+def main():
+    args = read_flags()
+    input_gen = read_std_in() if args.input == "-" else read_file(args.input)
+    incubators = vanilla() if args.vanilla else diversity()
+    if args.output != "-":
+        write_file(args.output, mpregnate(input_gen, incubators))
+    else:
+        write_std_out(mpregnate(input_gen, incubators))
+
 
 if __name__=="__main__":
-    print("MPREG")
-    mpreg(r'mpreg.txt', diversity=False)
-    print("MPREG Out")
+    main()
